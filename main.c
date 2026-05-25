0f58ef21b2c99c736b1e6dd688860b2d532a4ea4
Mitrofan <mityirg09@mail.ru>
2026-05-25 00:06:51 -0400

    Added prototype of function for pack, unpack, added open/close logic

--- main.c
+++ main.c
@@ -13,3 +13,41 @@ int main()
 
   return 0;
 }
+
+char* packFile(const char* fNameOrPath)
+{
+  if(!fNameOrPath) return NULL;
+
+  FILE* pF = fopen(fNameOrPath, "rb");
+  if(!pF) return NULL;
+  FILE* nF = fopen("pkdFile.txt", "wb");
+  if(!nF)
+  {
+    fclose(pF);
+    return NULL;
+  }
+
+  fclose(pF);
+  fclose(nF);
+
+  return "pkdFile.txt";
+}
+
+char* unpackFile(const char* fNameOrPath)
+{
+  if(!fNameOrPath) return NULL;
+
+  FILE* pkdF = fopen(fNameOrPath, "rb");
+  if(!pkdF) return NULL;
+  FILE* unpkdF = fopen("unpkdFile.txt", "wb");
+  if(!unpkdF)
+  {
+    fclose(pkdF);
+    return NULL;
+  }
+
+  fclose(unpkdF);
+  fclose(pkdF);
+
+  return "unpkdFile.txt";
+}
